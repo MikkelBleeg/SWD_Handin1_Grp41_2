5903dcbd6dc0d6ea480117e424161f465eaeb88b
Gnutz <kga@live.dk>
2018-10-16 13:24:56 +0200

    first viewModel changed

--- DebtBook/ViewModel_Layer/DebtBook_ViewModel.cs
+++ DebtBook/ViewModel_Layer/DebtBook_ViewModel.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ViewModel_Layer
+
+namespace DebtBook.ViewModel
 {
-    class DebtBook_ViewModel
+    public class DebtBook_ViewModel: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void INotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
