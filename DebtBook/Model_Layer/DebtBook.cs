a18bdb30ffac46795170f894a5a6a03eff7cb8a1
MikkelBleeg <mikkelbleeg@hotmail.com>
2018-10-16 12:54:01 +0200

    Added Everything

---
+++ DebtBook/Model_Layer/DebtBook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtBook.Model
+{
+    public class DebtBook
+    {
+        private ICollection<DebtHistory> Debts => new List<DebtHistory>();
+
+        public void AddNewHistory(string debtor = "", long initialAmount = 0)
+        {
+            Debts.Add(new DebtHistory(debtor, initialAmount));
+        }
+
+        public void AddDebit(string debtor = "", long amount = 0)
+        {
+            foreach (var history in Debts)
+            {
+                if (history.Debtor == debtor)
+                {
+                    history.AddDebit(amount);
+                }
+            }
+        }
+
+    }
+}
---
+++ DebtBook/Model_Layer/DebtHistory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtBook.Model
+{
+    public class DebtHistory
+    {
+        private ICollection<Debit> Debt => new List<Debit>();
+
+        public DebtHistory(string debtor = "", long initialAmount = 0)
+        {
+            Debtor = debtor;
+
+            if (initialAmount != 0)
+            {
+                Debt.Add(new Debit(initialAmount));
+            }
+
+        }
+        public string Debtor { get; set; }
+
+        public long Total
+        {
+            get
+            {
+                long total_ = 0;
+
+                foreach (var amount in Debt)
+                {
+                    total_ += amount.Amount;
+                }
+
+                return total_;
+            }
+        }
+
+        public void AddDebit(long amount)
+        {
+            Debt.Add(new Debit(amount));
+        }
+
+    }
+}
