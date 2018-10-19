a29d0d10f4544854ad6e59a0828bbae66ade821d
Mette <mettechrisjacobsen@hotmail.com>
2018-10-19 12:30:21 +0200

    Started on Modelview on adddebthis

---
+++ DebtBook/ViewModel_Layer/AddDebtHistory.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+using System.Xml.Serialization;
+using DebtBook.Model;
+using MvvmFoundation.Wpf;
+using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddDebtHistory.ViewModel
+{
+    public class AddDebtHistory_ViewModel :ObservableCollection<Debit>, INotifyPropertyChanged
+    {
+        private Model.DebtHistory DebtBook;
+        string filename = "";
+
+    private void INotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #region Commands
+
+        ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get { return _addCommand ?? (_addCommand = new RelayCommand(AddDebtHistory)); }
+        }
+
+        private void AddDebtHistory()
+        {
+            Add(new DebtHistory());
+            NotifyPropertyChanged("Count");
+            CurrentIndex = Count - 1;
+        }
+
+        ICommand _SaveCommand;
+        public ICommand SaveCommand
+        {
+            get { return _SaveCommand ?? (_SaveCommand = new RelayCommand(SaveFileCommand_Execute, SaveFileCommand_CanExecute)); }
+        }
+
+        private void SaveFileCommand_Execute()
+        {
+            // Create an instance of the XmlSerializer class and specify the type of object to serialize.
+            XmlSerializer serializer = new XmlSerializer(typeof(DebtHistory));
+            TextWriter writer = new StreamWriter(filename);
+            // Serialize all the Debit.
+            serializer.Serialize(writer, this);
+            writer.Close();
+        }
+
+        private bool SaveFileCommand_CanExecute()
+        {
+            return (filename != "") && (Count > 0);
+        }
+
+        private ICommand _CancelCommand;
+
+        public ICommand CancelCommand
+        {
+            //Do something
+        }
+
+        #endregion
+
+        #region Properties
+
+        int currentIndex = -1;
+
+        public int CurrentIndex
+        {
+            get { return currentIndex; }
+            set
+            {
+                if (currentIndex != value)
+                {
+                    currentIndex = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+
+}
