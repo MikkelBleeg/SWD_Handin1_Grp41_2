23e0bb8b0f671be29dbfb8b8e0bc30bd841d7db4
Mette <mettechrisjacobsen@hotmail.com>
2018-10-19 10:54:23 +0200

    Small changes

--- DebtBook/View_Layer/DebtHistory.xaml.cs
+++ DebtBook/View_Layer/DebtHistory.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace View_Layer
+namespace ViewLayer
 {
     /// <summary>
     /// Interaction logic for DebtHistory.xaml
